013c5cc331de4aab634f1eb4500d4485688b2f83
WDdeBWT <bwt1997@126.com>
2017-09-08 22:01:48 +0800

    0908-临时提交-评分页面

---
+++ WhutZongCeJiSuan/Controllers/EvaluateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhutZongCeJiSuan.Controllers
+{
+    public class EvaluateController : Controller
+    {
+        // GET: Evaluate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Score(int id)
+        {
+            return View();
+        }
+    }
+}
