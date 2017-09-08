9322d2440a7264f6cdc2e8975ca45b346df47a01
WDdeBWT <bwt1997@126.com>
2017-09-08 21:40:09 +0800

    0908-完成登陆功能

--- WhutZongCeJiSuan/Controllers/LoginController.cs
+++ WhutZongCeJiSuan/Controllers/LoginController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhutZongCeJiSuan.Models;
+using System.Text;
 
 namespace WhutZongCeJiSuan.Controllers
 {
     public class LoginController : Controller
     {
+        private ZongCeEntities db = new ZongCeEntities();
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string ID, string Psw)
+        {
+            var user = from T_Account in db.T_Account where (T_Account.ID == ID.ToString().Trim()) && (T_Account.password == Psw.ToString().Trim()) select T_Account;
+
+            if (user.Any() && user.Count() == 1)
+            {
+                //从数据集中提取
+                Session["UserID"] = user.First().ID;
+                Session["UserPsd"] = user.First().password;
+                return RedirectToAction("Index", "Account");
+            }
+            else
+            {
+                return Content("<script>alert('用户名或密码错误！请手动返回上一页重新登录');</script>");
+            }
         }
     }
 }
--- WhutZongCeJiSuan/Models/ZongCe.Context.cs
+++ WhutZongCeJiSuan/Models/ZongCe.Context.cs
@@ -13,10 +13,10 @@ namespace WhutZongCeJiSuan.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class ZongCeEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public ZongCeEntities()
+            : base("name=ZongCeEntities")
         {
         }
     
