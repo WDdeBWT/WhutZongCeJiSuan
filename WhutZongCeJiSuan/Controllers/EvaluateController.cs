c9e04a9e026c1b7c6c20e93154b167fef92ad879
WDdeBWT <bwt1997@126.com>
2017-09-09 14:44:15 +0800

    0909-添加评分页面

--- WhutZongCeJiSuan/Controllers/EvaluateController.cs
+++ WhutZongCeJiSuan/Controllers/EvaluateController.cs
@@ -13,8 +13,26 @@ namespace WhutZongCeJiSuan.Controllers
         {
             return View();
         }
-        public ActionResult Score(int id)
+
+        public ActionResult EvaForm(int id)
         {
+            ViewBag.EvaID = id;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EvaForm(string score1, string score2, string score3, string EvaID)
+        {
+            if ((score1 == null) || (score1 == null) || (score1 == null))
+            {
+                return Content("<script>alert('错误：请评价全部三项后再提交评分');history.go(-1);</script>");
+            }
+            float s1 = float.Parse(score1);
+            float s2 = float.Parse(score2);
+            float s3 = float.Parse(score3);
+            int eva_id = int.Parse(EvaID);
+            
             return View();
         }
     }
